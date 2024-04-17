8207ac25e067e9ff3f1a8c44beff450a595a9789
Amir Taheri <amir.taheri.web@protonmail.com>
2024-04-17 13:17:11 +0330

    chore: auth.js for hashing password and verifying password, token,... added

---
+++ src/utils/auth.js
@@ -0,0 +1,13 @@
+import { compare, hash } from "bcryptjs";
+
+const hashPassword = async (password) => {
+  const hashedPassword = await hash(password, 12);
+  return hashedPassword;
+};
+
+const verifyPassword = async (password, hashedPassword) => {
+  const isValid = await compare(password, hashedPassword);
+  return isValid;
+};
+
+export { hashPassword, verifyPassword };
