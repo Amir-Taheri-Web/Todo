d534c0a2f5b24b5144dffae944d081172ed80c79
Amir Taheri <amir.taheri.web@protonmail.com>
2024-04-17 15:43:35 +0330

    chore: axios configuration added

---
+++ src/configs/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "",
+  headers: { "Content-Type": "application/json" },
+});
+
+api.interceptors.response.use(
+  (response) => response.data,
+  (error) => Promise.reject(error)
+);
