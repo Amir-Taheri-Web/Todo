9095832961026795be012d71abc24ac4d52a4967
Amir Taheri <amir.taheri.web@protonmail.com>
2024-04-17 12:42:50 +0330

    chore: connectDB.js for connecting to database added

---
+++ src/utils/connectDB.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const connectDB = async (res) => {
+  try {
+    if (mongoose.connections[0].readyState) return;
+
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log("Connection to DB failed");
+
+    if (res)
+      res.status(500).json({
+        code: 500,
+        status: "failure",
+        message: "Failed to connect to database",
+      });
+  }
+};
+
+export default connectDB;
