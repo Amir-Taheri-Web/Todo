2c79f9f20ab768d3ea7ddbbfac221cb80e867ca8
Amir Taheri <amir.taheri.web@protonmail.com>
2024-04-17 12:17:03 +0330

    style: layout style added

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,44 +2,47 @@ import Link from "next/link";
 import { FaListCheck } from "react-icons/fa6";
 import { MdAddCircle } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
+import styles from "@/styles/Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <header>
+      <header className={styles.header}>
         <Link href="/">
           <h1>Todo App</h1>
         </Link>
       </header>
 
-      <aside>
-        <p>Welcome 👋</p>
+      <div className={styles.container}>
+        <aside className={styles.aside}>
+          <p>Welcome 👋</p>
 
-        <ul>
-          <li>
-            <Link href="/">
-              <FaListCheck />
-              Todos
-            </Link>
-          </li>
+          <ul>
+            <li>
+              <Link href="/">
+                <FaListCheck />
+                Todos
+              </Link>
+            </li>
 
-          <li>
-            <Link href="/add-todo">
-              <MdAddCircle />
-              Add Todo
-            </Link>
-          </li>
+            <li>
+              <Link href="/add-todo">
+                <MdAddCircle />
+                Add Todo
+              </Link>
+            </li>
 
-          <li>
-            <Link href="/profile">
-              <FaUser />
-              Profile
-            </Link>
-          </li>
-        </ul>
-      </aside>
+            <li>
+              <Link href="/profile">
+                <FaUser />
+                Profile
+              </Link>
+            </li>
+          </ul>
+        </aside>
 
-      <main>{children}</main>
+        <main className={styles.main}>{children}</main>
+      </div>
     </>
   );
 };
