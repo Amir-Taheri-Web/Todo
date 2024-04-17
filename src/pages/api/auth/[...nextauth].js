a8dfe5505bf9d7572250aec8b3dc4f021bf15d53
Amir Taheri <amir.taheri.web@protonmail.com>
2024-04-17 18:21:53 +0330

    feat: signin api with credentials created

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,45 @@
+import User from "@/models/user";
+import { verifyPassword } from "@/utils/auth";
+import connectDB from "@/utils/connectDB";
+import { validateEmail, validatePassword } from "@/utils/validate";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const authOptions = {
+  session: { strategy: "jwt" },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials, req) {
+        try {
+          await connectDB();
+        } catch (error) {
+          throw new Error("Connection to database failed");
+        }
+
+        const { email, password } = credentials;
+
+        if (!email.trim() || !email.trim())
+          throw new Error("Please fill all fields");
+
+        if (!validateEmail(email)) throw new Error("Email is not valid");
+
+        if (!validatePassword(password))
+          throw new Error(
+            "Password must be minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character(~`! @#$%^&*()-_+={}[]|;:<>,./?)"
+          );
+
+        const user = await User.findOne({ email: email });
+
+        if (!user) throw new Error("Email does not exist");
+
+        const isValid = await verifyPassword(password, user.password);
+
+        if (!isValid) throw new Error("Email or password is incorrect");
+
+        return { email };
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
