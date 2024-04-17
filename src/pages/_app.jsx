7f4fa4ea5990e2f6c9dc1926c26a8ca51b998d60
Amir Taheri <amir.taheri.web@protonmail.com>
2024-04-17 10:43:39 +0330

    style: font added to _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={montserrat.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
