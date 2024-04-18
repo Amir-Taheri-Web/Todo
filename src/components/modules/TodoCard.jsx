eb54433a5539f77109089d5b5720619257e4fd73
Amir Taheri <amir.taheri.web@protonmail.com>
2024-04-18 13:42:01 +0330

    feat: back and next buttons for each todo added

--- src/components/modules/TodoCard.jsx
+++ src/components/modules/TodoCard.jsx
@@ -1,6 +1,6 @@
 import styles from "@/styles/TodoCard.module.css";
 
-const TodoCard = ({ todos, status }) => {
+const TodoCard = ({ todos, status, back, next, statusHandler }) => {
   return (
     <ul className={styles.list}>
       {todos[status]?.map((todo) => (
@@ -8,6 +8,24 @@ const TodoCard = ({ todos, status }) => {
           <h4>{todo.title}</h4>
           <span className={styles[status]}></span>
           <p>{todo.description}</p>
+          <div>
+            {back && (
+              <button
+                type="button"
+                onClick={() => statusHandler(back, todo._id)}
+              >
+                Back
+              </button>
+            )}
+            {next && (
+              <button
+                type="button"
+                onClick={() => statusHandler(next, todo._id)}
+              >
+                Next
+              </button>
+            )}
+          </div>
         </li>
       ))}
     </ul>
--- src/components/templates/TodosPage.jsx
+++ src/components/templates/TodosPage.jsx
@@ -24,6 +24,26 @@ const TodosPage = () => {
     }
   };
 
+  const fetchTodos2 = async () => {
+    try {
+      const res = await api.get("/api/todos");
+
+      if (res.status === "success") setTodos(res.data);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const statusHandler = async (status, id) => {
+    try {
+      const res = await api.patch("api/todos", { status, id });
+
+      if (res.status === "success") fetchTodos2();
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   useEffect(() => {
     fetchTodos();
   }, []);
@@ -34,22 +54,44 @@ const TodosPage = () => {
     <div className={styles.container}>
       <div>
         <h3 className={styles.todo}>Todo</h3>
-        <TodoCard todos={todos} status="todo" />
+        <TodoCard
+          todos={todos}
+          status="todo"
+          statusHandler={statusHandler}
+          next="inProgress"
+        />
       </div>
 
       <div>
         <h3 className={styles.inProgress}>In Progress</h3>
-        <TodoCard todos={todos} status="inProgress" />
+        <TodoCard
+          todos={todos}
+          status="inProgress"
+          statusHandler={statusHandler}
+          back="todo"
+          next="review"
+        />
       </div>
 
       <div>
         <h3 className={styles.review}>Review</h3>
-        <TodoCard todos={todos} status="review" />
+        <TodoCard
+          todos={todos}
+          status="review"
+          statusHandler={statusHandler}
+          back="inProgress"
+          next="done"
+        />
       </div>
 
       <div>
         <h3 className={styles.done}>Done</h3>
-        <TodoCard todos={todos} status="done" />
+        <TodoCard
+          todos={todos}
+          status="done"
+          statusHandler={statusHandler}
+          back="review"
+        />
       </div>
     </div>
   );
