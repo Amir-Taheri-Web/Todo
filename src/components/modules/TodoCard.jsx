8c6711ddd182d568c7fda4e779ea0877596ce9ca
Amir Taheri <amir.taheri.web@protonmail.com>
2024-04-18 13:00:57 +0330

    style: Todo page style added

--- src/components/modules/TodoCard.jsx
+++ src/components/modules/TodoCard.jsx
@@ -1,9 +1,12 @@
+import styles from "@/styles/TodoCard.module.css";
+
 const TodoCard = ({ todos, status }) => {
   return (
-    <ul>
+    <ul className={styles.list}>
       {todos[status]?.map((todo) => (
         <li key={todo._id}>
           <h4>{todo.title}</h4>
+          <span className={styles[status]}></span>
           <p>{todo.description}</p>
         </li>
       ))}
--- src/components/templates/TodosPage.jsx
+++ src/components/templates/TodosPage.jsx
@@ -3,6 +3,7 @@ import Loader from "../modules/Loader";
 import toast from "react-hot-toast";
 import api from "@/configs/axios";
 import TodoCard from "../modules/TodoCard";
+import styles from "@/styles/TodosPage.module.css";
 
 const TodosPage = () => {
   const [todos, setTodos] = useState({});
@@ -30,24 +31,24 @@ const TodosPage = () => {
   if (isLoading) return <Loader />;
 
   return (
-    <div>
+    <div className={styles.container}>
       <div>
-        <h3>Todo</h3>
+        <h3 className={styles.todo}>Todo</h3>
         <TodoCard todos={todos} status="todo" />
       </div>
 
       <div>
-        <h3>In Progress</h3>
+        <h3 className={styles.inProgress}>In Progress</h3>
         <TodoCard todos={todos} status="inProgress" />
       </div>
 
       <div>
-        <h3>Review</h3>
+        <h3 className={styles.review}>Review</h3>
         <TodoCard todos={todos} status="review" />
       </div>
 
       <div>
-        <h3>Done</h3>
+        <h3 className={styles.done}>Done</h3>
         <TodoCard todos={todos} status="done" />
       </div>
     </div>
