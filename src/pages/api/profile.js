4f83b47f9d12d9e492230d31d5d8b13a00064e82
Amir Taheri <amir.taheri.web@protonmail.com>
2024-04-19 09:52:16 +0330

    feat: profile POST api created

---
+++ src/pages/api/profile.js
@@ -0,0 +1,47 @@
+import User from "@/models/user";
+import connectDB from "@/utils/connectDB";
+import { getSession } from "next-auth/react";
+
+const handler = async (req, res) => {
+  await connectDB(res);
+
+  const session = await getSession({ req });
+
+  if (!session)
+    return res
+      .status(401)
+      .json({ code: 401, status: "failure", message: "You are not logged in" });
+
+  const user = await User.findOne({ email: session.user.email });
+
+  if (!user)
+    return res.status(404).json({
+      code: 404,
+      status: "failure",
+      message: "You are unauthorized. Login again",
+    });
+
+  if (req.method === "POST") {
+    const { firstName, lastName } = req.body;
+
+    try {
+      user.firstName = firstName;
+      user.lastName = lastName;
+      await user.save();
+
+      return res
+        .status(200)
+        .json({ code: 200, status: "success", message: "Profile updated" });
+    } catch (error) {
+      console.log(error);
+
+      return res.status(500).json({
+        code: 500,
+        status: "failure",
+        message: "Connection to server failed",
+      });
+    }
+  }
+};
+
+export default handler;
